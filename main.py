68be2d65fa0f57458e98ee45f01824943d459b75
AmitDavidi <amitdavidi12@gmail.com>
2021-10-07 15:40:24 +0300

    Optimized code

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ sys.setrecursionlimit(25200)
 CLOCK = pygame.time.Clock()
 cwd = os.getcwd()
 images = os.path.join(cwd, "Resources")
+print(images)
 
-HEIGHT = 700
-WIDTH = 700
+HEIGHT = 1000
+WIDTH = 1000
 COLS = 8
 KNIGHTS_TOUR_COLS = COLS
 
@@ -1748,3 +1749,23 @@ try:
     main(WIN)
 except FileNotFoundError:
     print("your working directory is not set")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#1. d4 Nf6 2. Nf3 d5 3. e3 Bf5 4. c4 c6 5. Nc3 e6 6. Bd3 Bxd3 7. Qxd3 Nbd7 8. b3 Bd6 9. O-O O-O 10. Bb2 Qe7 11. Rad1 Rad8 12. Rfe1 dxc4 13. bxc4 e5 14. dxe5 Nxe5 15. Nxe5 Bxe5 16. Qe2 Rxd1 17. Rxd1 Rd8 18. Rxd8+ Qxd8 19. Qd1 Qxd1+ 20. Nxd1 Bxb2 21. Nxb2 b5 22. f3 Kf8 23. Kf2 Ke7  
+
+
+
+
